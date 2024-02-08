5434125846c082ed7308b125d0ba68cfd618c74f
slice3984 <31313749+slice3984@users.noreply.github.com>
2024-02-08 15:35:55 +0100

    Drawing rectangle / Shader & program compilation helpers

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
-#include <cstdio>
+#include <string>
+#include <format>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include "utils.h"
 
 // Window dimensions
 const GLint WIDTH = 800;
 const GLint HEIGHT = 600;
+
+void framebufferSizeCallback(GLFWwindow* window, GLint width, GLint height);
+void processInput(GLFWwindow* window);
 
 int main() {
     // Init GLFW
@@ -15,17 +20,16 @@ int main() {
         return -1;
     }
 
-    // GLFW window properties
     // OpenGL version
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    GLFWwindow* mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "My OpenGL App", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "My OpenGL App", nullptr, nullptr);
 
-    if (!mainWindow) {
-        printf("GLFW window creation failed!");
+    if (!window) {
+        std::cout << "GLFW window creation failed!\n";
         glfwTerminate();
 
         return -1;
@@ -33,14 +37,14 @@ int main() {
 
     int bufferWidth, bufferHeight;
 
-    glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
+    glfwGetFramebufferSize(window, &bufferWidth, &bufferHeight);
 
     // GLEW context
-    glfwMakeContextCurrent(mainWindow);
+    glfwMakeContextCurrent(window);
 
     if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
-        printf("Failed to init GLAD");
-        glfwDestroyWindow(mainWindow);
+        std::cout << "Failed to init GLAD\n";
+        glfwDestroyWindow(window);
         glfwTerminate();
 
         return -1;
@@ -48,18 +52,82 @@ int main() {
 
     glViewport(0, 0, bufferWidth, bufferHeight);
 
+    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+    float vertices[] = {
+            0.5f,  0.5f, 0.0f,  // top right
+            0.5f, -0.5f, 0.0f,  // bottom right
+            -0.5f, -0.5f, 0.0f,  // bottom left
+            -0.5f,  0.5f, 0.0f   // top left
+    };
+    unsigned int indices[] = {  // note that we start from 0!
+            0, 1, 3,   // first triangle
+            1, 2, 3    // second triangle
+    };
+
+    GLuint vertShader = utils::compileShader("shaders/vert.glsl", GL_VERTEX_SHADER);
+    GLuint fragShader = utils::compileShader("shaders/frag.glsl", GL_FRAGMENT_SHADER);
+    GLuint shaderProgram = utils::createProgram(vertShader, fragShader);
+
+    // VAO
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    // VBO
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+
+    // Copy over data (VBO)
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // EBO
+    GLuint ebo;
+    glGenBuffers(1, &ebo);
+
+    // Bind and copy over
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // Can safely unbind the buffer now, attrib ptr stored in vao
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // Unbind VAO
+    glBindVertexArray(0);
+
+    // Wireframe
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
     // Loop until window closed
-    while (!glfwWindowShouldClose(mainWindow)) {
+    while (!glfwWindowShouldClose(window)) {
         // Get & handle user input events
-        glfwPollEvents();
+        processInput(window);
 
         // Clear window
-        glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glfwSwapBuffers(mainWindow);
+        glUseProgram(shaderProgram);
+        glBindVertexArray(vao);
+        // glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
+        glfwSwapBuffers(window);
+        glfwPollEvents();
     }
 
+    glfwTerminate();
     return 0;
+}
+
+void framebufferSizeCallback(GLFWwindow* window, GLint width, GLint height) {
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+
 }
---
+++ utils.h
@@ -0,0 +1,19 @@
+//
+// Created by Sebastian on 07.02.2024.
+//
+
+#ifndef OPENGL_UTILS_H
+#define OPENGL_UTILS_H
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <format>
+#include <glad/glad.h>
+
+
+namespace utils {
+    GLuint compileShader(std::string const& path, GLenum shaderType);
+    GLuint createProgram(GLuint vertShader, GLuint fragShader);
+}
+
+#endif //OPENGL_UTILS_H
